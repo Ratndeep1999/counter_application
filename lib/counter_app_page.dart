95621af7acbf58e18b90988b84a454f6b758c1a3
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-16 21:28:33 +0530

    counter_app_page.dart : AppBar(icon, title, bg color) added, SafeArea() added, Container(decoration, text) added,

--- lib/counter_app_page.dart
+++ lib/counter_app_page.dart
@@ -8,11 +8,40 @@ class CounterAppPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // AppBar
       appBar: AppBar(
         leading: Icon(Icons.menu),
         centerTitle: true,
         title: Text("Counter App"),
         backgroundColor: Colors.grey,
+      ),
+      backgroundColor: Colors.grey.shade200,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+
+            // Display Number
+            Container(
+              height: MediaQuery.of(context).size.height * 0.25,
+              width: MediaQuery.of(context).size.width * 0.5,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Center(
+                child: Text(
+                  "0",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 150,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
