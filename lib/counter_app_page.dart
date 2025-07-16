457fef028f6ec0a550302143faee33f89a425e70
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-16 20:55:39 +0530

    counter_app_page.dart : class add, AppBar add, SafeArea add, container for male and female add,

--- lib/counter_app_page.dart
+++ lib/counter_app_page.dart
@@ -15,22 +15,47 @@ class CounterAppPage extends StatelessWidget {
         backgroundColor: Colors.orange.shade100,
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height ,
-                  width: MediaQuery.of(context).size.width ,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.grey
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              SizedBox(height: 15),
+
+              // Gender Section
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.20,
+                      width: MediaQuery.of(context).size.width * 0.10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
                   ),
-                ),
-                Container(),
-              ],
-            ),
-          ],
+
+                  SizedBox(width: 15),
+
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.20,
+                      width: MediaQuery.of(context).size.width * 0.10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey.shade300,
+                      ),
+                      child: Icon(
+                        Icons.female,
+                        size: 100,
+                        color: Colors.pink.shade100,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
